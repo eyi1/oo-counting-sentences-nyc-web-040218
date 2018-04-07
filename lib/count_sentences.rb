af4ac31d3d67038e4ea07dccc15d9793688ed30d
EJ <EJ@tests-MacBook-Pro-3.local>
2018-04-07 16:24:17 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+      
+    end
+
   end
-
+ 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false 
   end
 
   def count_sentences
-
+  
+    self.split(/[.?!]/).reject(&:empty?).length
+    # complex.delete("").length
+      #remove empty strings from array. 
   end
 end
